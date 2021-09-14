6ca252e557d194aaeb515c5d7b31836851d0e938
Pedro Henrique Sotto-Mayor Pereira da Silva <itapeu16@hotmail.com>
2021-09-13 18:17:00 -0700

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner sc= new Scanner(System.in);
+        String euros, rate;
+        double val_euros, val_rate;
+        double total;
+
+        System.out.print("How many euros are you exchanging? ");
+        euros=sc.nextLine();
+        System.out.print("What is the exchange rate? ");
+        rate=sc.nextLine();
+
+        val_euros=Double.parseDouble(euros);
+        val_rate=Double.parseDouble(rate);
+
+        total=val_euros*val_rate;
+
+        System.out.printf("%.2f euros at an exchange rate of %.2f is\n", val_euros, val_rate);
+        System.out.printf("%.2f U.S. dollars", total);
+
+
+    }
+}
